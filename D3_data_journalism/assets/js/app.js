92f1128f60fabb1c0626082977b1fa4e93c0e22a
njokuann <njokuann@gmail.com>
2019-11-16 06:11:09 -0600

    push

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -78,7 +78,7 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     //Create axis labels
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
+        .attr("y", 0 - margin.left + 40)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "axisText")
